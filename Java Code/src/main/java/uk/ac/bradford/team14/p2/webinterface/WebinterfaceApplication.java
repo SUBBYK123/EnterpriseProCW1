7a62fe3e5477bbbc151d9e0ba1b3abe1f0398a4d
hkamran2 <hamza_kamran@ymail.com>
2025-02-14 00:43:57 +0000

    Backend Code

---
+++ Java Code/src/main/java/uk/ac/bradford/team14/p2/webinterface/WebinterfaceApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.bradford.team14.p2.webinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebinterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebinterfaceApplication.class, args);
+    }
+
+}
---
+++ Java Code/src/test/java/uk/ac/bradford/team14/p2/webinterface/WebinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.bradford.team14.p2.webinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebinterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
