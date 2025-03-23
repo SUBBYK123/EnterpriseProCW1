e9269d5bcf26ee53a29e0bf5b757267af02451c8
Mohammad Subhaan Khurram <mohammadsubhaankhurram1@gmail.com>
2025-03-23 16:01:01 +0000

    - updated MyDB

--- Database/MyDB.sql
+++ Database/MyDB.sql
@@ -16,28 +16,57 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `datasets`
+-- Table structure for table `activity_logs`
 --
 
-DROP TABLE IF EXISTS `datasets`;
+DROP TABLE IF EXISTS `activity_logs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `datasets` (
-  `dataset_id` int NOT NULL AUTO_INCREMENT,
-  `dataset_name` varchar(100) NOT NULL,
-  `description` text,
-  PRIMARY KEY (`dataset_id`),
-  UNIQUE KEY `dataset_name` (`dataset_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `activity_logs` (
+  `LogID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int DEFAULT NULL,
+  `RoleID` int DEFAULT NULL,
+  `Action` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`LogID`),
+  KEY `UserID` (`UserID`),
+  KEY `RoleID` (`RoleID`),
+  CONSTRAINT `activity_logs_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `datasets`
+-- Dumping data for table `activity_logs`
 --
 
-LOCK TABLES `datasets` WRITE;
-/*!40000 ALTER TABLE `datasets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `datasets` ENABLE KEYS */;
+LOCK TABLES `activity_logs` WRITE;
+/*!40000 ALTER TABLE `activity_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `activity_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dataset_index`
+--
+
+DROP TABLE IF EXISTS `dataset_index`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dataset_index` (
+  `DatasetID` int NOT NULL AUTO_INCREMENT,
+  `DatasetName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `RoleID` int DEFAULT NULL,
+  `Timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`DatasetID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dataset_index`
+--
+
+LOCK TABLES `dataset_index` WRITE;
+/*!40000 ALTER TABLE `dataset_index` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dataset_index` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -48,18 +77,15 @@ DROP TABLE IF EXISTS `permission_requests`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `permission_requests` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `role_id` int NOT NULL,
-  `status` enum('PENDING','APPROVED','DENIED') DEFAULT 'PENDING',
-  `request_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `decision_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `permission_requests_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `permission_requests_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `requestid` int NOT NULL AUTO_INCREMENT,
+  `userid` int NOT NULL,
+  `status` enum('PENDING','APPROVED','DENIED') COLLATE utf8mb4_general_ci DEFAULT 'PENDING',
+  `request_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `decision_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`requestid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `permission_requests_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`UserID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,111 +98,34 @@ LOCK TABLES `permission_requests` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `role_datasets`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `role_datasets`;
+DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `role_datasets` (
-  `role_id` int NOT NULL,
-  `dataset_id` int NOT NULL,
-  PRIMARY KEY (`role_id`,`dataset_id`),
-  KEY `dataset_id` (`dataset_id`),
-  CONSTRAINT `role_datasets_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
-  CONSTRAINT `role_datasets_ibfk_2` FOREIGN KEY (`dataset_id`) REFERENCES `datasets` (`dataset_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role_datasets`
---
-
-LOCK TABLES `role_datasets` WRITE;
-/*!40000 ALTER TABLE `role_datasets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `role_datasets` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `roles` (
-  `role_id` int NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(50) NOT NULL,
-  `department` varchar(50) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`role_id`),
-  UNIQUE KEY `role_name` (`role_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles`
---
-
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
+CREATE TABLE `users` (
+  `UserID` int NOT NULL AUTO_INCREMENT,
+  `First_Name` varchar(50) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `RoleID` int DEFAULT NULL,
+  `User_Access` enum('Admin','User') DEFAULT 'User',
   `email_address` varchar(100) DEFAULT NULL,
   `password_hash` varchar(255) DEFAULT NULL,
   `department` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `email_address_UNIQUE` (`email_address`),
-  UNIQUE KEY `password_hash_UNIQUE` (`password_hash`)
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `email_address_UNIQUE` (`email_address`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'m','k','mk@test.com','Password123','adult'),(2,'Mustafa','Kamran','mustafakamran46@hotmail.com','$2a$10$Pu83qNYHL9xN9iF/WFRVmO42VtOQaE/zW9rWvAGb5CMGvyTralmhm','adult'),(3,'Mustafa','Kamran','mustafakamran461@hotmail.com','$2a$10$ZCgi/EjKRhuHxo8HNDntsuLXOyRE98n1aNfADykZpP.vP9af145wq','adult'),(4,'Mohammad Subhaan','Khurram','mohammadsubhaankhurram1@gmail.com','$2a$10$UrVjJybylRWzoXtrWAO9b.e1oJ/1Z1rbuK3euHmSckFg/Mzd98acG','adult');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_roles`
---
-
-DROP TABLE IF EXISTS `user_roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_roles` (
-  `user_id` int NOT NULL,
-  `role_id` int NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_roles`
---
-
-LOCK TABLES `user_roles` WRITE;
-/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'m','k',NULL,'User','mk@test.com','Password123','adult'),(2,'Mustafa','Kamran',NULL,'User','mustafakamran46@hotmail.com','$2a$10$Pu83qNYHL9xN9iF/WFRVmO42VtOQaE/zW9rWvAGb5CMGvyTralmhm','adult'),(3,'Mustafa','Kamran',NULL,'User','mustafakamran461@hotmail.com','$2a$10$ZCgi/EjKRhuHxo8HNDntsuLXOyRE98n1aNfADykZpP.vP9af145wq','adult'),(4,'Mohammad Subhaan','Khurram',NULL,'User','mohammadsubhaankhurram1@gmail.com','$2a$10$UrVjJybylRWzoXtrWAO9b.e1oJ/1Z1rbuK3euHmSckFg/Mzd98acG','adult');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -188,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-16 17:41:51
+-- Dump completed on 2025-03-23 14:45:42
